a1def600e4bc97bce9f9d86e85d249c81511ae3d
Subhajit <subha.pramanik1212@gmail.com>
2022-08-02 22:01:03 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountDetailsComponent } from './features/account-details/account-details.component';
+import { AccountComponent } from './features/account/account.component';
+import { ContainerComponent } from './features/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'oms',
+    component: ContainerComponent,
+    children: [
+      {
+        path: 'account',
+        component: AccountComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/oms',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './features/account/account.component';
+import { AccountDetailsComponent } from './features/account-details/account-details.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ContainerComponent } from './features/container/container.component';
+import { SearchBoxComponent } from './shared/search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    AccountDetailsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContainerComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
